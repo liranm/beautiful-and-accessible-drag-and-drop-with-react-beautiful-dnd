920f49a0ecb1f10236ef6e555fca5c594b774118
Liran Mery <liranm@ironsrc.com>
2018-11-11 08:57:53 +0200

    Optimize Performance in react-beautiful-dnd with shouldComponentUpdate and PureComponent

--- src/column.jsx
+++ src/column.jsx
@@ -18,10 +18,30 @@ const Title = styled.h3`
 const TaskList = styled.div`
     padding: 8px;
     transition: background-color 0.2s ease;
-    background-color: ${props => (props.isDraggingOver ? 'skyblue': 'inherit')};
+    background-color: ${props => (props.isDraggingOver ? 'lightgrey': 'inherit')};
     flex-grow: 1;
     min-height: 100px;
 `;
+
+class InnerList extends React.Component {
+    shouldComponentUpdate(nextProps) {
+        if(nextProps.tasks === this.props.tasks) {
+            return false;
+        }
+
+        return true;
+    }
+
+    render() {
+        return this.props.tasks.map((task ,index) => 
+            <Task 
+                key={task.id} 
+                task={task}
+                index={index} 
+            />
+        );
+    }
+}
 
 export default class Column extends React.Component {
     render() {
@@ -49,13 +69,14 @@ export default class Column extends React.Component {
                                     {...provided.droppableProps}
                                     isDraggingOver={snapshot.isDraggingOver}
                                 >
-                                    {this.props.tasks.map((task ,index) => 
+                                    <InnerList tasks={this.props.tasks}/>
+                                    {/* {this.props.tasks.map((task ,index) => 
                                         <Task 
                                             key={task.id} 
                                             task={task}
                                             index={index} 
                                         />
-                                    )}
+                                    )} */}
                                     {provided.placeholder}
                                 </TaskList>
                             )}
--- src/index.js
+++ src/index.js
@@ -10,6 +10,15 @@ const Container = styled.div`
     display: flex;
 `;
 
+class InnerList extends React.PureComponent {
+    render() {
+        const { column, taskMap, index } = this.props;
+        const tasks = column.taskIds.map(taskId => taskMap[taskId]);
+
+        return <Column column={column} tasks={tasks} index={index} />
+    }
+}
+
 class App extends React.Component {
     state = initialData;
 
@@ -115,15 +124,12 @@ class App extends React.Component {
                         >
                             {this.state.columnOrder.map((columnId, index) => {
                                 const column = this.state.columns[columnId];
-                                const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
-                                const isDropDisabled = index < this.state.homeIndex;
                     
                                 return (
-                                    <Column
+                                    <InnerList
                                         key={column.id}
                                         column={column}
-                                        tasks={tasks}
-                                        isDropDisabled={isDropDisabled}
+                                        taskMap={this.state.tasks}
                                         index={index}
                                     />
                                 );
